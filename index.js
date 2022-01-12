08a267925726682a14e0ce5d33f76d0b4726be05
Rohit barche <rohitbarche99@gmail.com>
2022-01-12 20:46:00 +0530

    Init

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require("fs")
+
+// creting new file 
+
+
+// fs.writeFileSync('read.txt',"welcome to my tphit mm");
+
+//fs.appendFileSync("read.txt","how are you  i am fine");
+
+// fs.readFileSync('read.txt')
+
+// const buf_data  = fs.readFileSync("read.txt")
+
+// console.log(buf_data);
+
+// const org_data = buf_data.toString();
+// console.log(org_data);
+
+//fs.renameSync('read.txt',"readwrite.txt")
